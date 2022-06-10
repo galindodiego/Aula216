736e87c3b496223a5649d4f88fbb7b14430ceaf1
Diego Galindo <diegogalindo84@gmail.com>
2022-06-10 16:19:46 -0300

    Criada a lógica na classe ContractService

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import services.ContractService;
 import services.OnlinePaymentService;
 import services.PaypalService;
 
 public class Program {
 
 	public static void main (String[] args) {
+		
+		OnlinePaymentService ps = new PaypalService();
+		
+		ContractService cs = new ContractService(ps);
 
 	}
 }
